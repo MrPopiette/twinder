9a7ce1ed06f1015936c9474b0c7946b282cf2a3b
Enkyu <benjamin.strabach@live.fr>
2021-01-10 15:25:56 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Purge Arioflow',
+      title: 'Twinder',
       theme: darkTheme,
       initialRoute: '/',
       onGenerateRoute: RouteGenerator.generateRoute,
