5734508378079dcc1123a6520db7d1d91d2dfe79
Benjamin Strabach <benjamin.strabach@live.fr>
2021-04-25 19:28:17 +0200

    fix overflowed errors on login and register views

--- lib/ui/views/login.dart
+++ lib/ui/views/login.dart
@@ -14,52 +14,54 @@ class LoginView extends StatelessWidget {
             fit: BoxFit.cover,
           ),
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              child: Center(
-                child: Container(
-                  margin: EdgeInsets.all(48),
-                  child: Image(image: AssetImage('assets/images/logoTwinder.png')),
-                ),
-              ),
-            ),
-            Container(
-              child: Center(
-                child: Container(
-                  margin: EdgeInsets.all(32),
-                  child: AuthForm(
-                    type: FormType.login,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                child: Center(
+                  child: Container(
+                    margin: EdgeInsets.all(48),
+                    child: Image(image: AssetImage('assets/images/logoTwinder.png')),
                   ),
                 ),
               ),
-            ),
-            RichText(
-                text: TextSpan(style: TextStyle(fontSize: 16), children: [
-              TextSpan(text: "Pas de Compte ? "),
-              TextSpan(
-                  text: "S'inscrire",
-                  style: TextStyle(color: Colors.pinkAccent, fontWeight: FontWeight.bold),
-                  recognizer: TapGestureRecognizer()
-                    ..onTap = () {
-                      Navigator.of(context).pushNamed(
-                        '/register',
-                        arguments: "Passage d'argument",
-                      );
-                    }),
-            ]))
-            // Text("Login Screen"),
-            // RaisedButton(
-            //   child: Text('Timeline'),
-            //   onPressed: () {
-            //     Navigator.of(context).pushNamed(
-            //       '/timeline',
-            //       arguments: "Passage d'argument",
-            //     );
-            //   },
-            // ),
-          ],
+              Container(
+                child: Center(
+                  child: Container(
+                    margin: EdgeInsets.all(32),
+                    child: AuthForm(
+                      type: FormType.login,
+                    ),
+                  ),
+                ),
+              ),
+              RichText(
+                  text: TextSpan(style: TextStyle(fontSize: 16), children: [
+                TextSpan(text: "Pas de Compte ? "),
+                TextSpan(
+                    text: "S'inscrire",
+                    style: TextStyle(color: Colors.pinkAccent, fontWeight: FontWeight.bold),
+                    recognizer: TapGestureRecognizer()
+                      ..onTap = () {
+                        Navigator.of(context).pushNamed(
+                          '/register',
+                          arguments: "Passage d'argument",
+                        );
+                      }),
+              ]))
+              // Text("Login Screen"),
+              // RaisedButton(
+              //   child: Text('Timeline'),
+              //   onPressed: () {
+              //     Navigator.of(context).pushNamed(
+              //       '/timeline',
+              //       arguments: "Passage d'argument",
+              //     );
+              //   },
+              // ),
+            ],
+          ),
         ),
       ),
     );
--- lib/ui/views/register.dart
+++ lib/ui/views/register.dart
@@ -19,36 +19,38 @@ class RegisterView extends StatelessWidget {
             fit: BoxFit.cover,
           ),
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              child: Center(
-                child: Container(
-                  margin: EdgeInsets.all(32),
-                  child: AuthForm(
-                    type: FormType.register,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                child: Center(
+                  child: Container(
+                    margin: EdgeInsets.all(32),
+                    child: AuthForm(
+                      type: FormType.register,
+                    ),
                   ),
                 ),
               ),
-            ),
-            RichText(
-              textAlign: TextAlign.center,
-              text: TextSpan(style: TextStyle(fontSize: 14), children: [
-                TextSpan(text: "En cliquant sur Inscription, vous reconnaissez avoir lu et approuvé les "),
-                TextSpan(
-                  text: "Conditions générales d'utilisation",
-                  style: TextStyle(color: Colors.deepPurpleAccent, fontWeight: FontWeight.bold),
-                ),
-                TextSpan(text: " et la "),
-                TextSpan(
-                  text: "Politique de confidentialité",
-                  style: TextStyle(color: Colors.deepPurpleAccent, fontWeight: FontWeight.bold),
-                ),
-                TextSpan(text: "."),
-              ]),
-            )
-          ],
+              RichText(
+                textAlign: TextAlign.center,
+                text: TextSpan(style: TextStyle(fontSize: 14), children: [
+                  TextSpan(text: "En cliquant sur Inscription, vous reconnaissez avoir lu et approuvé les "),
+                  TextSpan(
+                    text: "Conditions générales d'utilisation",
+                    style: TextStyle(color: Colors.deepPurpleAccent, fontWeight: FontWeight.bold),
+                  ),
+                  TextSpan(text: " et la "),
+                  TextSpan(
+                    text: "Politique de confidentialité",
+                    style: TextStyle(color: Colors.deepPurpleAccent, fontWeight: FontWeight.bold),
+                  ),
+                  TextSpan(text: "."),
+                ]),
+              )
+            ],
+          ),
         ),
       ),
     );
